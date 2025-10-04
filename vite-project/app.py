6fa42d8cccf2a7c678497bf65b3009824be6dcd2
Gabriel Howe <me@gabe.how>
2025-10-04 12:55:53 -0400

    add working app.py

--- vite-project/app.py
+++ vite-project/app.py
@@ -1,17 +1,18 @@
+import datetime
 import os
-
+from flask import Flask, render_template, request, jsonify
+from google import genai
 import ee
-import google.generativeai as genai
-from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
+PROJECT_ID = "deft-cove-474115-j1"
+API_KEY = "AIzaSyDjN3wQtKA3BWuQVyeQSJdMwui7kao4-Rg"
 
-ee.Initialize(project="lucid-fountain-466101-v0")
+ee.Initialize(project=PROJECT_ID)
 
-genai.configure(api_key="AIzaSyDjN3wQtKA3BWuQVyeQSJdMwui7kao4-Rg")
+client = genai.Client(api_key=API_KEY)
 
 MODEL_NAME = "gemini-2.5-flash"
-
 
 def get_tile_url(polarization="VV", mode="IW"):
     point = ee.Geometry.Point([-90.0715, 29.9511])
@@ -33,7 +34,6 @@ def get_tile_url(polarization="VV", mode="IW"):
         print("Error generating tile URL:", e)
         return None
 
-
 @app.route("/")
 def index():
     polarization = request.args.get("polarization", "VV")
@@ -50,29 +50,31 @@ def index():
         message=message,
     )
 
-
-@app.route("/describe")
+@app.route("/describe", methods=["GET"])
 def describe():
     polarization = request.args.get("polarization", "VV")
     mode = request.args.get("mode", "IW")
+    print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] Request: polarization={polarization}, mode={mode}")
+    
     prompt = f"Describe a Sentinel-1 SAR image with polarization {polarization} in mode {mode}."
+    
     try:
-        model = genai.GenerativeModel("gemini-2.5-flash")
-        response = model.generate_content(prompt)
-        description = response.text
-        return jsonify({"description": description})
-    except Exception as e:
-        import traceback
-
-        traceback.print_exc()
-        return jsonify(
-            {"description": f"Error generating Gemini description: {str(e)}"}
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
         )
+        return jsonify({"description": response.text})
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"description": f"Error generating description: {e}"})
 
-
-@app.route("/historical")
-def historical():
-    return render_template("historical.html")
+@app.route("/clicked")
+def clicked():
+    lat = request.args.get("lat")
+    lng = request.args.get("lng")
+    return jsonify({lat,lng})
+    
 
 
 if __name__ == "__main__":
